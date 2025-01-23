953d547de50c8499942116ac95447a6909665aad
Valentin Afonso <valentinafonso22@gmail.com>
2025-01-23 23:14:45 +0100

    ⚡️add countup

--- src/components/GithubCounter.tsx
+++ src/components/GithubCounter.tsx
@@ -1,9 +1,39 @@
+"use client";
+import { useState } from "react";
+import CountUp from "react-countup";
+
 export default function GithubCounter({ counter }: any) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
   // 1 + 8 + 160 + 254 + 473 + 20 = 916
   return (
-    <div className="absolute top-0 left-0 w-full h-full blurry_link flex justify-center items-center rounded-xl px-4 gap-2 opacity-0 pointer-events-none group-hover/herolink:opacity-100 transition-all">
+    <div
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="absolute top-0 left-0 w-full h-full blurry_link flex justify-center items-center rounded-xl px-4 gap-2 opacity-0 group-hover/herolink:opacity-100 transition-all"
+    >
       <div className="flex flex-col">
-        <span className="text-lg font-bold">+{counter}</span>
+        <div className="text-lg font-bold">
+          +
+          {isHovered ? (
+            <CountUp
+              start={0}
+              end={counter}
+              duration={2}
+              key={isHovered.toString()}
+            />
+          ) : (
+            <>0</>
+          )}
+        </div>
+
         <span className="text-xs text-black/60 dark:text-white/60">
           contributions
         </span>
--- src/components/LeetcodeCounter.tsx
+++ src/components/LeetcodeCounter.tsx
@@ -1,23 +1,41 @@
+"use client";
 import LottieAnimation from "./LottieAnimation";
-// import NumberFlow from "@number-flow/react";
+import { useState } from "react";
+import CountUp from "react-countup";
 
-export default async function LeetcodeCounter({ counter }: any) {
-  /*
-  const data = await fetch(
-    "https://leetcode-api-faisalshohag.vercel.app/valafso"
-  );
-  if (!data) return;
-  const leetcode = await data.json();
-  if (!leetcode) return;
-  {leetcode.totalSolved}
-  */
+export default function LeetcodeCounter({ counter }: any) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
   return (
-    <div className="absolute top-0 left-0 w-full h-full blurry_link flex justify-center items-center px-4 gap-2 opacity-0 rounded-xl pointer-events-none group-hover/herolink:opacity-100 transition-all">
+    <div
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="absolute top-0 left-0 w-full h-full blurry_link flex justify-center items-center px-4 gap-2 opacity-0 rounded-xl group-hover/herolink:opacity-100 transition-all"
+    >
       <div className="flex flex-col gap-1 mt-[-16px]">
         <div className="flex items-center justify-center ml-[-6px]">
           <LottieAnimation animationPath="/lottie/fire-animation.json" />
-          <span className="text-lg font-bold mb-[-16px]">+{counter}</span>
-          {/*<NumberFlow value={123} /> **/}
+          <div className="text-lg font-bold mb-[-16px]">
+            +
+            {isHovered ? (
+              <CountUp
+                start={0}
+                end={counter}
+                duration={2}
+                key={isHovered.toString()}
+                className="text-lg font-bold mb-[-16px]"
+              />
+            ) : (
+              <>0</>
+            )}
+          </div>
         </div>
         <span className="text-xs text-black/60 dark:text-white/60">
           problems solved
